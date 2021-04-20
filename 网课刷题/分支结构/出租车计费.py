555a67d96ea664337bf7f0e444420bae6fd1d9c7
WhiteSheep-y <xiaomie_y@163.com>
2021-04-20 19:24:48 +0800

    master

--- 网课刷题/分支结构/出租车计费.py
+++ 网课刷题/分支结构/出租车计费.py
@@ -1,11 +1,9 @@
 import math
-mileage,time = input().split(',')
-mileage=eval(mileage)
-time=eval(time)
+mileage,time = eval(input())
 if mileage<=3 :
     price = 13
 elif mileage<=15 :
-    price = math.fsum([13,(mileage-3)*2.3])
+    price = 13+(mileage-3)*2.3
 else:
-    price = math.fsum([13,(mileage-3)*2.3,(mileage-15)*2.3*1.5])
-print("{:d}".format(math.floor(price+time)))
+    price = 13+12*2.3+(mileage-15)*2.3*1.5
+print(int(price+time))
